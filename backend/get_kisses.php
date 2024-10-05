8fc5676a916f89cf2c4e40dbe439536034b37a43
Felipe Faggion <felipe.faggion.ufsc@gmail.com>
2024-10-04 21:35:35 -0300

    test

--- backend/get_kisses.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'kisses.txt'; // Mude o caminho para subir um nível
-if (file_exists($file)) {
-    echo file_get_contents($file);
-} else {
-    echo '0'; // Caso o arquivo não exista ainda
-}
-?>
